2c1468c6f345e613188d76c21ccc3c7dace247f5
後閑諒一 <nargok@gmail.com>
2017-07-05 22:29:11 +0900

    RSS Dataを取得処理を追加

---
+++ newsReaderApp/Item.swift
@@ -0,0 +1,12 @@
+//
+//  Item.swift
+//  newsReaderApp
+//
+//  Created by 後閑諒一 on 2017/07/05.
+//  Copyright © 2017年 ryoichi.gokan. All rights reserved.
+//
+
+class Item {
+    var title = ""
+    var link = ""
+}
